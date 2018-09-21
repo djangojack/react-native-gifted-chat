6e5ad837d0b8991f1dbaec7242ee502fc4270078
djangojack <luke@luke-2.local>
2018-09-20 20:13:21 -0400

    fix android keyboard covering input

--- src/InputToolbar.js
+++ src/InputToolbar.js
@@ -2,7 +2,7 @@
 
 import PropTypes from 'prop-types';
 import React from 'react';
-import { StyleSheet, View, Keyboard, ViewPropTypes } from 'react-native';
+import { StyleSheet, Platform, View, Keyboard, ViewPropTypes } from 'react-native';
 
 import Composer from './Composer';
 import Send from './Send';
@@ -23,8 +23,12 @@ export default class InputToolbar extends React.Component {
   }
 
   componentWillMount() {
-    this.keyboardWillShowListener = Keyboard.addListener('keyboardWillShow', this.keyboardWillShow);
-    this.keyboardWillHideListener = Keyboard.addListener('keyboardWillHide', this.keyboardWillHide);
+
+    const showEvent = Platform.OS === 'android' ? 'keyboardDidShow' : 'keyboardWillShow';
+    const hideEvent = Platform.OS === 'android' ? 'keyboardDidHide' : 'keyboardWillHide';
+    
+    this.keyboardWillShowListener = Keyboard.addListener(showEvent, this.keyboardWillShow);
+    this.keyboardWillHideListener = Keyboard.addListener(hideEvent, this.keyboardWillHide);
   }
 
   componentWillUnmount() {
